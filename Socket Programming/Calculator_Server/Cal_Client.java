1f498efed3bcb9546b38fb7c18faab24a22cac15
Shyaamal Tripathi <shyaamal1108@gmail.com>
2021-06-12 07:48:41 +0530

    Add files via upload

---
+++ Socket Programming/Calculator_Server/Cal_Client.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.BufferedReader;
+public class Cal_Client
+{
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println();
+        System.out.println("The client has started...");
+        Socket soc = new Socket("localhost", 9806);//Creating a socket object with an IP address and a port number
+        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+        PrintWriter out= new PrintWriter(soc.getOutputStream(), true);
+
+        int ch, num_1, num_2;
+        do
+        {
+            System.out.println("Please choose the operation from the list of options");
+            System.out.println("1. Addition");
+            System.out.println();
+            System.out.println("2. Subtraction");
+            System.out.println();
+            System.out.println("3. Multiplication");
+            System.out.println();
+            System.out.println("4. Divison");
+            System.out.println();
+            System.out.println("5. Exit");
+            System.out.println();
+            ch=Integer.parseInt(userInput.readLine());
+            if(ch==5)
+            {
+                out.println(ch+":0:0");
+                break;
+            }
+            else
+            {
+
+            System.out.println("Please enter the first number: ");
+            num_1=Integer.parseInt(userInput.readLine());
+            System.out.println();
+            System.out.println("Please enter the second number: ");
+            num_2=Integer.parseInt(userInput.readLine());
+            out.println(ch+":"+num_1+":"+num_2);
+
+            String answer=in.readLine();
+            System.out.println("Server says: "+answer);
+            
+        } 
+
+    }while(true);
+}
+
+}
+
+
---
+++ Socket Programming/Calculator_Server/Cal_Server.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.BufferedReader;
+public class Cal_Server 
+{
+    public static void main(String[] args) throws Exception
+    {
+        ServerSocket ss=new ServerSocket(9806);
+        System.out.println("Waiting for the client.....");
+        System.out.println();
+        Socket soc=ss.accept();
+        System.out.println("The connection is established between with the client");
+        System.out.println();
+        BufferedReader in=new BufferedReader(new InputStreamReader(soc.getInputStream()));// Reading data from client
+        PrintWriter out=new PrintWriter(soc.getOutputStream(), true);
+        
+        while(true)
+        {
+            String str[]=in.readLine().split(":");// split into different parts based on the delimeter storing each part in string array
+            int option=Integer.parseInt(str[0]);
+            int num1=Integer.parseInt(str[1]);
+            int num2=Integer.parseInt(str[2]);
+            String result="";
+
+            switch(option)
+            {
+                case 1: 
+                result="Addition is:"+(num1+num2);
+                break;
+                
+                case 2: 
+                result="Subtraction is:"+(num1-num2);
+                break;
+
+                case 3: 
+                result="Multiplication is:"+(num1*num2);
+                break;
+
+                case 4: 
+                result="Divison is:"+(num1/(double)num2);
+                break;
+
+                case 5: 
+                System.exit(0);
+
+                default: 
+                break;
+            }
+            out.println(result);
+        }
+    }
+    
+}
