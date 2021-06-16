6e70cbe34cdcb99f4e102592f327b443366a791a
Shyaamal Tripathi <shyaamal1108@gmail.com>
2021-06-16 20:46:50 +0530

    Add files via upload

---
+++ Java_Projects/TwoDim.java
@@ -0,0 +1,23 @@
+package P1;
+public class TwoDim
+{
+    private int x;
+    private int y;
+    
+    public TwoDim()
+    {
+        this.x=0;
+        this.y=0;
+    }
+    public TwoDim(int x, int y)
+    {
+        this.x=x;
+        this.y=y;
+    }
+    public String toString()
+    {
+        return ("Value of x coordinate is:"+x+"\nValue of y coordinate is:"+y);
+    }
+
+    
+}
